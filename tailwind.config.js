a8cc687c2584bc690364127990f6a9e2b903bcd2
Ahmed Eid <ahmedeidworker@outlook.com>
2024-09-12 02:36:42 +0300

    Project Code

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        green: "hsl(75, 94%, 57%)",
+        white: "hsl(0, 0%, 100%)",
+        "grey-700": "hsl(0, 0%, 20%)",
+        "grey-800": "hsl(0, 0%, 12%)",
+        "grey-900": "hsl(0, 0%, 8%)",
+      },
+    },
+  },
+  plugins: [],
+};
